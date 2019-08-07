66f2de8b410291050edb147e26ff37514f86fe39
piyushnh <piyush.n.h@gmail.com>
2019-08-07 16:04:54 +0530

    Trying flows out

--- rotato/apps/socialAuth/models.py
+++ rotato/apps/socialAuth/models.py
@@ -1,3 +1,12 @@
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+except ImportError:
+    from django.contrib.auth.models import User
 from django.db import models
+# from oauth2client.contrib.django_util.models import CredentialsField
 
-# Create your models here.
+
+# class CredentialsModel(models.Model):
+#   id = models.ForeignKey(User, primary_key=True, on_delete = models.CASCADE)
+#   credential = CredentialsField()
--- rotato/apps/socialAuth/views.py
+++ rotato/apps/socialAuth/views.py
@@ -8,46 +8,114 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from requests.exceptions import HTTPError
-
+from django.shortcuts import redirect
 from social_django.utils import psa
 
 from apiclient import discovery
 import httplib2
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+
 from oauth2client import client
+# from oauth2client.contrib.django_util.storage import DjangoORMStorage
+
+
+
 import os
 import json
+
+CLIENT_SECRETS_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "client_secret.json")
+SCOPES = ['https://www.googleapis.com/auth/drive.appdata', 'profile', 'email']
+
+@api_view()
+@permission_classes([AllowAny])
+def exchange_auth_code(request):
+                # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
+        flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+            CLIENT_SECRETS_FILE, scopes=SCOPES)
+
+        # The URI created here must exactly match one of the authorized redirect URIs
+        # for the OAuth 2.0 client, which you configured in the API Console. If this
+        # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
+        # error.
+        flow.redirect_uri = "http://localhost:8000/auth/social/exchange_auth/"
+
+        authorization_url, state = flow.authorization_url(
+            # Enable offline access so that you can refresh an access token without
+            # re-prompting the user for permission. Recommended for web server apps.
+            access_type='offline',
+            # Enable incremental authorization. Recommended as a best practice.
+            include_granted_scopes='true')
+
+        # Store the state so the callback can verify the auth server response.
+        request.session['state'] = state
+        return redirect(authorization_url)
+
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def exchange_auth_code(request):
-        # if not request.headers.get('X-Requested-With'):
-        #     abort(403)
+        print(request.data)
+    #     # if not request.headers.get('X-Requested-With'):
+    #     #     abort(403)
 
+    #     auth_code = request.data['code']
+
+    #     # Set path to the Web application client_secret_*.json file you downloaded from the
+    #     # Google API Console: https://console.developers.google.com/apis/credentials
+
+        
+    #     # Exchange auth code for access token, refresh token, and ID token
+    #     # credentials = client.credentials_from_clientsecrets_and_code(
+    #     #     CLIENT_SECRET_FILE,
+    #     #     ['https://www.googleapis.com/auth/drive.appdata', 'profile', 'email'],
+    #     #     auth_code)
+
+    #     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+    #   CLIENT_SECRET_FILE, scopes=SCOPES )
+    #     flow.redirect_uri='postmessage'
+
+    #     # access_token = credentials.token_response['access_token']
+    #     flow.fetch_token(code=auth_code)
+    #     credentials = flow.credentials
+
+    #     # Call Google API
+    #     # http_auth = credentials.authorize(httplib2.Http())
+    #     # drive_service = discovery.build('drive', 'v3', http=http_auth)
+    #     # appfolder = drive_service.files().get(fileId='appfolder').execute()
+
+    #     # Get profile info from ID token
+    #     # userid = credentials.id_token['sub']
+    #     # email = credentials.id_token['email']
+
+    #     request.session['credentials'] = credentials
+
+    #     # credentials = json.dumps(credentials)
+    #     # response = redirect('exchange_token', backend='google-oauth2')
+
+    #     # return response
+    #     return Response(credentials.to_json(),status=status.HTTP_200_OK)
+
+            # Specify the state when creating the flow in the callback so that it can
+        # verified in the authorization server response.
+        # state = request.session['state']
+
+        flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+            CLIENT_SECRETS_FILE, scopes=None)
+        flow.redirect_uri = "postmessage"
+
+        # Use the authorization server's response to fetch the OAuth 2.0 tokens.
         auth_code = request.data['code']
+        flow.fetch_token(code = auth_code)
 
-        # Set path to the Web application client_secret_*.json file you downloaded from the
-        # Google API Console: https://console.developers.google.com/apis/credentials
+        # Store credentials in the session.
+        # ACTION ITEM: In a production app, you likely want to save these
+        #              credentials in a persistent database instead.
+        credentials = flow.credentials
+        # request.session['credentials'] = credentials
 
-        CLIENT_SECRET_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "client_secret.json")
+        return Response('kjniu',status=status.HTTP_200_OK)
 
-        # Exchange auth code for access token, refresh token, and ID token
-        credentials = client.credentials_from_clientsecrets_and_code(
-            CLIENT_SECRET_FILE,
-            ['https://www.googleapis.com/auth/drive.appdata', 'profile', 'email'],
-            auth_code)
-
-        access_token = credentials.token_response['access_token']
-        # Call Google API
-        # http_auth = credentials.authorize(httplib2.Http())
-        # drive_service = discovery.build('drive', 'v3', http=http_auth)
-        # appfolder = drive_service.files().get(fileId='appfolder').execute()
-
-        # Get profile info from ID token
-        userid = credentials.id_token['sub']
-        email = credentials.id_token['email']
-
-        # credentials = json.dumps(credentials)
-        return Response( credentials.to_json(),status=status.HTTP_200_OK)
 
 
 
