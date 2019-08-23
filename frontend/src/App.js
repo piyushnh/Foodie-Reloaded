519ffe00e1eb1123ee8968f5dd79017df518424a
naitemach <npsnayak@gmail.com>
2019-08-23 13:25:34 +0530

    added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import Navbar from './components/Navigation/Navbar';
-import BottomNavbar from './components/Navigation/BottomNavbar';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { withStyles } from '@material-ui/core/styles';
-import GenericError from './components/ErrorHandlers/GenericError';
+import React, { Component } from "react";
+import Navbar from "./components/Navigation/Navbar";
+import BottomNavbar from "./components/Navigation/BottomNavbar";
+import { BrowserRouter as Router } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
 
-// redux imports
-import { connect } from 'react-redux';
-import * as actions from './store/actions/auth/action';
-import LoginDialog from './components/Authentication/LoginOptions'
+// redux importss
+import { connect } from "react-redux";
+import * as actions from "./store/actions/auth/action";
+import LoginDialog from "./components/Authentication/LoginOptions";
 
-import BaseRouter from './routes';
+import BaseRouter from "./routes";
+import { Typography } from "antd";
+// import BottomScrollListener from "react-bottom-scroll-listener";
 
 const styles = theme => ({
-  root: {
-    // display: 'flex', 
-    // flexWrap: 'wrap',  
-
-  },
-
+	root: {
+		// display: 'flex',
+		// flexWrap: 'wrap',
+	}
 });
 
 class App extends Component {
+<<<<<<< 0971fb46f9b7b4c9fd7506f1e6e2b35c26102b36
 
   constructor(props) {
     super(props);
@@ -61,13 +61,57 @@ class App extends Component {
 
     );
   }
+=======
+	constructor(props) {
+		super(props);
+		this.state = {
+			openLoginDialog: true
+		};
+
+		this.handleLoginDialogClose = this.handleLoginDialogClose.bind(this);
+	}
+
+	handleLoginDialogClose() {
+		console.log("Closed");
+		this.setState({ openLoginDialog: false });
+	}
+
+	componentDidMount() {
+		// this.props.onTryAutoSignup();
+	}
+
+	render() {
+		const { classes } = this.props;
+		return (
+			<Router>
+				<div className={classes.root}>
+					<Navbar {...this.props} />
+
+					<BaseRouter />
+
+					{this.props.isAuthenticated ? (
+						/* (
+						<BottomScrollListener />
+          )  */
+						<Typography />
+					) : (
+						<LoginDialog
+							open={this.state.openLoginDialog}
+							handleClose={this.handleLoginDialogClose}
+						/>
+					)}
+				</div>
+			</Router>
+		);
+	}
+>>>>>>> added
 }
 
 const mapStateToProps = state => {
-  return {
-    isAuthenticated: state.authReducer.token !== null
-  }
-}
+	return {
+		isAuthenticated: state.authReducer.token !== null
+	};
+};
 
 // const mapDispatchToProps = dispatch => {
 //   return {
@@ -75,4 +119,7 @@ const mapStateToProps = state => {
 //   }
 // }
 
-export default connect(mapStateToProps, null)(withStyles(styles)(App));
+export default connect(
+	mapStateToProps,
+	null
+)(withStyles(styles)(App));
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-
-import Users from './components/users' ;
+import RouteWithLayout from "./components/RouteWithLayout";
+import Users from "./components/users";
 import RestaurantList from "./components/Restaurant/RestaurantList";
 import MenuGridList from "./components/Menu/Menu";
 import Cart from "./components/Orders/Cart";
@@ -11,21 +11,65 @@ import MerchantLogin from "./components/MerchantView/Login";
 import FoodCourtList from "./components/FoodCourt/FoodCourtList";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
+import { Main as MainLayout, Minimal as MinimalLayout } from "./layouts";
+
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "./theme";
 
 const BaseRouter = () => (
-  <Switch>
-      <Route exact path="/users/" component={Users} />
-      <Route exact path="/nearbyfoodcourts/" component={FoodCourtList} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/merchant/login" component={MerchantLogin} />
-      <Route exact path="/signup" component={Signup} />
-      <Route exact path='/nearbyfoodcourts/:food_name' name="food_court_restaurants" component={RestaurantList} />
-      <Route exact path='/foodcourts/:restaurant_name/menu' name="food_court_restaurants" component={MenuGridList} />
-      <Route exact path='/foodcourts/:restaurant_name/order/' name="food_court_restaurants_order" component={Cart} />
-      <Route exact path='/foodcourts/restaurants/payment' name="food_court_restaurants_payment" component={PaymentOptions} />
-      <Route exact path='/foodcourts/order/response' name="food_court_restaurants_response" component={OrderResponse} />
-
-  </Switch>
+	<Switch>
+		<Route exact path="/users/" component={Users} />
+		<ThemeProvider theme={theme}>
+			<RouteWithLayout
+				exact
+				path="/nearbyfoodcourts/"
+				component={FoodCourtList}
+				layout={MainLayout}
+			/>
+			<RouteWithLayout
+				exact
+				path="/login"
+				component={Login}
+				layout={MainLayout}
+			/>
+			<RouteWithLayout
+				exact
+				path="/signup"
+				component={Signup}
+				layout={MainLayout}
+			/>
+		</ThemeProvider>
+		<Route
+			exact
+			path="/nearbyfoodcourts/:food_name"
+			name="food_court_restaurants"
+			component={RestaurantList}
+		/>
+		<Route
+			exact
+			path="/foodcourts/:restaurant_name/menu"
+			name="food_court_restaurants"
+			component={MenuGridList}
+		/>
+		<Route
+			exact
+			path="/foodcourts/:restaurant_name/order/"
+			name="food_court_restaurants_order"
+			component={Cart}
+		/>
+		<Route
+			exact
+			path="/foodcourts/restaurants/payment"
+			name="food_court_restaurants_payment"
+			component={PaymentOptions}
+		/>
+		<Route
+			exact
+			path="/foodcourts/order/response"
+			name="food_court_restaurants_response"
+			component={OrderResponse}
+		/>
+	</Switch>
 );
 
 export default BaseRouter;
